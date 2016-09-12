f787245624ca63066c90119688502453d21c91a2
Nick Kubala <nkubala@google.com>
2016-09-12 14:36:06 -0700

    refactor to pull out tag retrieval into helper method. no functional change

--- tag_dedupe/dedupe.py
+++ tag_dedupe/dedupe.py
@@ -8,9 +8,32 @@ import argparse
 AUTH_FILE_PATH_LOCAL = "/auth.json"
 
 def dedupe(input_tag, existing_tags):
+	print "Existing tags for image {0}:".format(image_path)
+	for tag in existing_tags:
+		print tag
+
 	if input_tag in existing_tags:
 		sys.exit("Tag already exists in remote repository! Exiting build.")
 	print "Tag does not exist in remote repository! Continuing with build."
+
+def retrieve_tags(raw_image_path):
+	# extract both path to image, and the tag if provided
+	image_parts = raw_image_path.split(':')
+	image_path = image_parts[0]
+	if len(image_parts) == 2:
+		image_tag = image_parts[1]
+	else:
+		image_tag = 'latest'
+
+	p = subprocess.Popen(["/builder/google-cloud-sdk/bin/gcloud alpha container images list-tags --format='value(tags)' {0}".format(image_path)],
+		shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+	output, error = p.communicate()
+	if p.returncode != 0:
+		sys.exit("Error encountered when retrieving existing image tags! Full log: \n\n" + output)
+
+	existing_tags = set(tag.rstrip() for tag in output.split('\n'))
+	return existing_tags
 
 
 # Since gcloud auth information doesn't transfer automatically into a new container in prod, 
@@ -42,6 +65,7 @@ def login(AUTH_FILE_PATH_REMOTE):
 
 	os.remove(AUTH_FILE_PATH_LOCAL)
 
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--image', type=str, help='Fully qualified remote path for the target image')
@@ -50,26 +74,7 @@ def main():
 
 	login(args.auth_file)
 
-	# extract both path to image, and the tag if provided
-	image_parts = args.image.split(':')
-	image_path = image_parts[0]
-	if len(image_parts) == 2:
-		image_tag = image_parts[1]
-	else:
-		image_tag = 'latest'
-
-	p = subprocess.Popen(["/builder/google-cloud-sdk/bin/gcloud alpha container images list-tags --format='value(tags)' {0}".format(image_path)],
-		shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-	output, error = p.communicate()
-	if p.returncode != 0:
-		sys.exit("Error encountered when retrieving existing image tags! Full log: \n\n" + output)
-
-	existing_tags = set(tag.rstrip() for tag in output.split('\n'))
-
-	print "Existing tags for image {0}:".format(image_path)
-	for tag in existing_tags:
-		print tag
+	existing_tags = retrieve_tags(args.image)
 
 	dedupe(image_tag, existing_tags)
 
