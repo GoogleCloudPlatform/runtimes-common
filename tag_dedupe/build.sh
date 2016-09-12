c095df16dee6271d8cbf6114bcded2b7ce78e0f7
Nick Kubala <nkubala@google.com>
2016-09-12 12:46:05 -0700

    add image path and remote auth file path as arguments to both scripts.

--- tag_dedupe/build.sh
+++ tag_dedupe/build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 set -e
-set -x
 
 IMAGE=$1
+REMOTE_AUTH_FILE_PATH=$2
+
+if [ -z "$1" ]; then
+	echo "Usage: ./build.sh [image_path] [auth_file_remote_path]"
+	echo "Please provide fully qualified path to target image."
+	exit 1
+fi
+
+if [ -z "$2" ]; then
+	echo "Path to remote auth file path not provided; using auth file bundled into container!"
+fi
 
 sed -i "s|%IMAGE%|$IMAGE|g" cloudbuild.yaml
+sed -i "s|%AUTH_FILE%|$AUTH_FILE|g" cloudbuild.yaml
 gcloud alpha container builds create . --config=cloudbuild.yaml
--- tag_dedupe/dedupe.py
+++ tag_dedupe/dedupe.py
@@ -3,9 +3,9 @@
 import sys
 import subprocess
 import os
+import argparse
 from sets import Set
 
-AUTH_FILE_PATH_REMOTE = "gs://nick-cloudbuild-auth/nick-cloudbuild-743a54b95573.json"
 AUTH_FILE_PATH_LOCAL = "/auth.json"
 
 def dedupe(input_tag, existing_tags):
@@ -17,18 +17,22 @@ def dedupe(input_tag, existing_tags):
 
 # Since gcloud auth information doesn't transfer automatically into a new container in prod, 
 # we use a placeholder service account so we can make authenticated calls to the gcloud API.
-def login():
+def login(AUTH_FILE_PATH_REMOTE):
 	#######################################################################################
 	# TODO: once we can make unauthenticated gsutil calls in prod, 
 	# uncomment this and remove the temporary auth.json file from the docker image.
 
-	# p1 = subprocess.Popen(["/builder/google-cloud-sdk/bin/gsutil cp {0} {1}".format(AUTH_FILE_PATH_REMOTE, AUTH_FILE_PATH_LOCAL)],
-	# 	shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	# if AUTH_FILE_PATH_REMOTE != '':
+	# 	p1 = subprocess.Popen(["/builder/google-cloud-sdk/bin/gsutil cp {0} {1}".format(AUTH_FILE_PATH_REMOTE, AUTH_FILE_PATH_LOCAL)],
+	# 		shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-	# for line in p1.stdout.readlines():
-	# 	print line
-	# 	if "ERROR" in line or "Exception" in line:
-	# 		sys.exit("Error encountered when retrieving gcloud credentials. Exiting build. ({0})".format(line))
+	# 	for line in p1.stdout.readlines():
+	# 		print line
+	# 		if "ERROR" in line or "Exception" in line:
+	# 			sys.exit("Error encountered when retrieving gcloud credentials. Exiting build. ({0})".format(line))
+	# else:
+	# 	print "Remote auth file not provided; defaulting to bundled auth file from container!"
+	# 	print "If this file was not provided when container image was built, THIS STEP WILL FAIL!"
 	#######################################################################################
 
 	p2 = subprocess.Popen(["/builder/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file {0}".format(AUTH_FILE_PATH_LOCAL)],
@@ -41,14 +45,15 @@ def login():
 	os.remove(AUTH_FILE_PATH_LOCAL)
 
 def main():
-	args = sys.argv[1:] # ignore script name
-	if len(args) < 1:
-		sys.exit('Please provide the qualified path to your image, in the form: $REPO_ID/$PROJECT_ID/$IMAGE_NAME:$TAG')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--image', type=str, help='Fully qualified remote path for the target image')
+	parser.add_argument('--auth_file', type=str, help='Path to JSON auth file in Google Cloud Storage associated with a service account with READ permissions to your image bucket.')
+	args = parser.parse_args()
 
-	login()
+	login(args.auth_file)
 
 	# extract both path to image, and the tag if provided
-	image_parts = args[0].split(':')
+	image_parts = args.image.split(':')
 	image_path = image_parts[0]
 	if len(image_parts) == 2:
 		image_tag = image_parts[1]
