549856af9a59ce1538f2067112d11aad29b90ee4
Nick Kubala <nkubala@users.noreply.github.com>
2016-09-16 14:19:11 -0400

    Add cloud build step to check for existing tag on target image before building (#1)
    * add build step to check for existing tag in remote repository before building, and exit build if it exists.
    
    * move tag dedupe files into directory
    
    * scrub auth
    
    * gitignore
    
    * add build script to replace image path in build config with provided image. rewrite step in yaml
    
    * remove json build config
    
    * comment dockerfile
    
    * add image path and remote auth file path as arguments to both scripts.
    
    * remove gitignore. update yaml to reference gcp-runtimes project. don't loop through set elements. use native set implementation.
    
    * use list comprehension over lambda
    
    * check return code of list-tags and exit/display error if nonzero
    
    * refactor to pull out tag retrieval into helper method. no functional change
    
    * move login into dockerfile command, remove python logic. rename file and method for clarity.
    
    * return boolean from helper method to main method for testing
    
    * remove old script
    
    * add travis config to run flake8
    
    * flake8 formatting
    
    * remove auth_file arg from python/bash in favor of bundled auth. rename target container. file/directory name refactoring.

---
+++ check_if_image_tag_exists/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys
+import subprocess
+import argparse
+
+AUTH_FILE_PATH_LOCAL = "/auth.json"
+
+
+def check_if_tag_exists(raw_image_path):
+    # extract both path to image, and the tag if provided
+    image_parts = raw_image_path.split(':')
+    image_path = image_parts[0]
+    if len(image_parts) == 2:
+        image_tag = image_parts[1]
+    else:
+        image_tag = 'latest'
+
+    p = subprocess.Popen(["/builder/google-cloud-sdk/bin/gcloud "
+                          + "alpha container images list-tags "
+                          + "--format='value(tags)' {0}".format(image_path)],
+                         shell=True, stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+
+    output, error = p.communicate()
+    if p.returncode != 0:
+        sys.exit("Error encountered when retrieving existing image tags! "
+                 + "Full log: \n\n" + output)
+
+    existing_tags = set(tag.rstrip() for tag in output.split('\n'))
+    print "Existing tags for image {0}:".format(image_path)
+    for tag in existing_tags:
+        print tag
+
+    if image_tag in existing_tags:
+        sys.exit("Tag already exists in remote repository! Exiting build.")
+    print "Tag does not exist in remote repository! Continuing with build."
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=str,
+                        help='Fully qualified remote path for the '
+                        + 'target image')
+    args = parser.parse_args()
+
+    check_if_tag_exists(args.image)
+
+if __name__ == "__main__":
+    main()
