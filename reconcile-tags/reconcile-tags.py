4717665e3c9a875db0ef0383060504562a6970ae
sharifelgamal <selgamal@google.com>
2016-10-05 11:27:19 -0700

    adding tag reconciler

---
+++ reconcile-tags/reconcile-tags.py
@@ -0,0 +1,31 @@
+"""Reads a json file mapping docker digests to tags and reconciles them.
+
+Reads tap-map.json, currently a file in the same folder as this script
+and parses the json. Calls gcloud beta container images add-tag on each entry.
+If there are no changes that api call is no-op.
+"""
+
+import glob
+import json
+from subprocess import call
+
+
+def main():
+  files = glob.glob('./*.json')
+  for f in files:
+    print f
+    with open(f) as tag_map:
+      data = json.load(tag_map)
+      for repo, images in data.items():
+        print repo
+        for image in images:
+          print image
+          digest = image['digest']
+          tag = image['tag']
+          print digest
+          print tag
+          call(['gcloud', 'beta', 'container', 'images',
+              'add-tag', repo+'@sha256:'+digest, repo+':'+tag, '-q'])
+
+if __name__ == '__main__':
+  main()
