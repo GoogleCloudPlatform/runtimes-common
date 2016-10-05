c0753455bc1f69c5de959fb76dcd47307ac6e5a1
sharifelgamal <selgamal@google.com>
2016-10-05 15:58:33 -0700

    cleaning up the script

--- reconcile-tags/reconcile-tags.py
+++ reconcile-tags/reconcile-tags.py
@@ -1,32 +1,43 @@
 """Reads a json file mapping docker digests to tags and reconciles them.
 
-Reads tap-map.json, currently a file in the same folder as this script
-and parses the json. Calls gcloud beta container images add-tag on each entry.
+Reads all json files in current directory and parses it into repositories
+and tags. Calls gcloud beta container images add-tag on each entry.
 If there are no changes that api call is no-op.
 """
 
+import argparse
 import glob
 import json
 from subprocess import call
 
 
-def main():
+def reconcile_tags(dry_run):
+    call(['gcloud', 'config', 'list'])
     files = glob.glob('./*.json')
     for f in files:
-        print f
+        print '\nProcessing {0}'.format(f)
         with open(f) as tag_map:
             data = json.load(tag_map)
             for repo, images in data.items():
-                print repo
                 for image in images:
-                    print image
                     digest = image['digest']
                     tag = image['tag']
-                    print digest
-                    print tag
-                    call(['gcloud', 'beta', 'container', 'images',
-                          'add-tag', repo+'@sha256:'+digest,
-                          repo+':'+tag, '-q'])
+                    full_digest = repo + '@sha256:' + digest
+                    full_tag = repo + ':' + tag
+                    print '\nTagging {0} with {1}'.format(full_digest, full_tag)
+                    if not dry_run:
+                        print ('Running gcloud beta container images '
+                               'add-tag {0} {1} -q'
+                               .format(full_digest, full_tag))
+                        call(['gcloud', 'beta', 'container', 'images',
+                              'add-tag', full_digest, full_tag, '-q'])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dryrun', action='store_true', default=False)
+    args = parser.parse_args()
+    reconcile_tags(args.dryrun)
 
 if __name__ == '__main__':
     main()
