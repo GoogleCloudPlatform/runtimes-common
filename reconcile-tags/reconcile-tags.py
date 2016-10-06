215079e7cabfd912a566cedbc3c743614e83ccd5
sharifelgamal <selgamal@google.com>
2016-10-06 12:47:47 -0700

    passing flake

--- reconcile-tags/reconcile-tags.py
+++ reconcile-tags/reconcile-tags.py
@@ -10,13 +10,16 @@ import glob
 import json
 import subprocess
 
+
 def call(digest, tag, dry_run):
     print '\nTagging {0} with {1}'.format(digest, tag)
     if not dry_run:
-        command = 'gcloud beta container images add-tag {0} {1} -q'.format(digest,tag)
+        command = 'gcloud beta container images add-tag'
+        ' {0} {1} -q'.format(digest, tag)
         print 'Running {0}'.format(command)
         output = subprocess.check_output([command], shell=True)
         print output
+
 
 def reconcile_tags(dry_run):
     subprocess.call(['gcloud', 'config', 'list'])
@@ -36,7 +39,8 @@ def reconcile_tags(dry_run):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dry-run', dest="dry_run", action='store_true', default=False)
+    parser.add_argument('--dry-run', dest="dry_run",
+                        action='store_true', default=False)
     args = parser.parse_args()
     reconcile_tags(args.dry_run)
 
