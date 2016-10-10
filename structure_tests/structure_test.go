cccbd9ded244f8984e642699fa6bccdcc53fd765
Nick Kubala <nkubala@google.com>
2016-10-10 10:36:10 -0700

    add structure_tests.go and necessary build components to build image for use as cloudbuild step

---
+++ structure_tests/structure_test.go
@@ -0,0 +1,100 @@
+package structure_tests
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"testing"
+	"encoding/json"
+	"flag"
+	"log"
+)
+
+type CommandTest struct {
+	Name				string		// name of test
+	Command				string 		// command to run
+	Flags				string 		// optional flags
+	ExpectedOutput		string 		// expected output of running command
+}
+
+type FileExistenceTest struct {
+	Name				string		// name of test
+	FilePath			string 		// file to check existence of
+	ShouldExist			bool 		// whether or not the file should exist
+}
+
+type FileContentTest struct {
+	Name				string		// name of test
+	FilePath			string 		// file to check existence of
+	ExpectedContents	string 		// expected contents of file	
+}
+
+type StructureTest struct {
+	Commands			[]CommandTest		`json:"commands"`
+	FileExistenceTests 	[]FileExistenceTest	`json:"file_existence"`
+	FileContentTests 	[]FileContentTest	`json:"file_contents"`
+}
+
+
+func TestRunCommand(t *testing.T) {
+	for _, tt := range tests.Commands {
+		t.Log(tt.Name)
+		var cmd *exec.Cmd
+		if tt.Flags != "" {
+			cmd = exec.Command(tt.Command, tt.Flags)
+		} else {
+			cmd = exec.Command(tt.Command)
+		}
+		actualOutput, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Errorf("Error running command: %s %s. Error: %s. Output %s.", tt.Command, tt.Flags, err, actualOutput)
+		}
+		if tt.ExpectedOutput != "" && string(actualOutput) != tt.ExpectedOutput {
+			t.Errorf("Unexpected output for command: %s\nExpected: %s\nActual: %s", tt.Command, tt.ExpectedOutput, string(actualOutput))
+		}
+	}
+}
+
+
+func TestFileExists(t *testing.T) {
+	for _, tt := range tests.FileExistenceTests {
+		t.Log(tt.Name)
+		output, err := ioutil.ReadDir(tt.FilePath)
+		if tt.ShouldExist && err != nil {
+			t.Errorf("File %s should exist but does not! Error: %s", tt.FilePath, output)
+		} else if !tt.ShouldExist && err == nil {
+			t.Errorf("File %s should not exist but does! Error: %s", tt.FilePath, output)
+		}
+	}
+}
+
+
+func TestFileContents(t *testing.T) {
+	for _, tt := range tests.FileContentTests {
+		t.Log(tt.Name)
+		actualContents, err := ioutil.ReadFile(tt.FilePath)
+		if err != nil {
+			t.Errorf("Failed to open %s. Error: %s", tt.FilePath, err)
+		}
+		if strings.TrimSpace(tt.ExpectedContents) != strings.TrimSpace(string(actualContents[:])) {
+			t.Errorf("Unexpected file contents encountered!\nExpected: %s\nActual: %s", tt.ExpectedContents, string(actualContents[:]))
+		}
+	}
+}
+
+var configFile string; var tests StructureTest
+func init() {
+	flag.StringVar(&configFile, "config", "/workspace/structure_test.json",
+		"path to the .yaml file containing test definitions.")
+	flag.Parse()
+
+	var err error; var testJson []byte
+	testJson, err = ioutil.ReadFile(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	marshalErr := json.Unmarshal(testJson, &tests)
+	if marshalErr != nil {
+		log.Fatal(err)
+	}
+}
