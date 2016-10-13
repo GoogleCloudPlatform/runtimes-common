a327b3e1392d68e216f6defd11f9bccc2ffae44f
Sharif Elgamal <selgamal@google.com>
2016-10-13 12:11:53 -0700

    adding tag reconciler (#6)
    * adding tag reconciler
    
    * fixing flake formatting failures
    
    * cleaning up the script
    
    * cleaning up the script
    
    * more script fixes
    
    * passing flake
    
    * adding deleting tag and stylistic changes
    
    * more structure changes
    
    * removing delete functionality and passing flake
    
    * formatting json
    
    * adding unit tests and running them in travis
    
    * fixing flake
    
    * style stuff

---
+++ reconciletags/reconciletags.py
@@ -0,0 +1,79 @@
+"""Reads json files mapping docker digests to tags and reconciles them.
+
+Reads all json files in current directory and parses it into repositories
+and tags. Calls gcloud beta container images add-tag on each entry.
+If there are no changes that api call is no-op.
+"""
+
+import argparse
+import json
+import logging
+import subprocess
+
+
+class TagReconciler:
+    def call(self, command, dry_run, fmt="json"):
+        command += " --format=" + fmt
+        if not dry_run:
+            logging.debug('Running {0}'.format(command))
+            output = subprocess.check_output([command], shell=True)
+            logging.debug(output)
+            return output
+        else:
+            logging.debug('Would have run {0}'.format(command))
+
+    def add_tags(self, digest, tag, dry_run):
+        logging.debug('Tagging {0} with {1}'.format(digest, tag))
+        command = ('gcloud beta container images add-tag {0} {1} '
+                   '-q'.format(digest, tag))
+        self.call(command, dry_run)
+
+    # This turns a list of lists into one flat list of tags
+    def flatten_tags_list(self, list_of_lists):
+        flat_tags_list = []
+        for sublist in list_of_lists:
+            for tag in sublist:
+                if tag:
+                    flat_tags_list.append(tag)
+        return flat_tags_list
+
+    def get_existing_tags(self, repo):
+        output = json.loads(self.call('gcloud beta container images list-tags '
+                            '--no-show-occurrences {0}'.format(repo), False))
+
+        list_of_tags = [image['tags'] for image in output]
+        existing_tags = self.flatten_tags_list(list_of_tags)
+        return existing_tags
+
+    def reconcile_tags(self, data, dry_run):
+        # Hardcode dry_run to False for this call because we always want
+        # want to see config regardless of whether we actually run the
+        # reconciler.
+        self.call('gcloud config list', False)
+        for repo, images in data.items():
+            logging.debug(self.get_existing_tags(repo))
+
+            for image in images:
+                full_digest = repo + '@sha256:' + image['digest']
+                full_tag = repo + ':' + image['tag']
+                self.add_tags(full_digest, full_tag, dry_run)
+
+            logging.debug(self.get_existing_tags(repo))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dry-run', dest="dry_run",
+                        action='store_true', default=False)
+    parser.add_argument('file',
+                        help='The file to run the reconciler on')
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG)
+    r = TagReconciler()
+    logging.debug('---Processing {0}---'.format(args.file))
+    with open(args.file) as tag_map:
+        data = json.load(tag_map)
+    r.reconcile_tags(data, args.dry_run)
+
+if __name__ == '__main__':
+    main()
---
+++ reconciletags/reconciletags_test.py
@@ -0,0 +1,83 @@
+"""Unit tests for reconcile-tags.py.
+
+Unit tests for reconcile-tags.py.
+"""
+import unittest
+import mock
+import reconciletags
+
+_REPO = 'gcr.io/foobar/baz'
+_DIGEST1 = 'digest1'
+_DIGEST2 = 'digest2'
+_TAG1 = 'tag1'
+_TAG2 = 'tag2'
+
+_LIST_RESP = """
+[
+  {
+    "digest": "sha256:digest1",
+    "tags": [
+      "tag1"
+    ],
+    "timestamp": {
+    }
+  }
+]
+"""
+
+_GCLOUD_CONFIG = 'gcloud config list --format=json'
+_GCLOUD_LIST = ('gcloud beta container images list-tags '
+                '--no-show-occurrences {0} --format=json'.format(_REPO))
+
+
+class ReconcileTagsTest(unittest.TestCase):
+
+    def _gcloudAdd(self, digest, tag):
+        return ('gcloud beta container images add-tag {0} {1} -q --format=json'
+                .format(_REPO+'@sha256:'+digest, _REPO+':'+tag))
+
+    def setUp(self):
+        self.r = reconciletags.TagReconciler()
+        self.data = {_REPO: [{'digest': _DIGEST1, 'tag': _TAG1}]}
+
+    def test_reconcile_tags(self):
+        with mock.patch('subprocess.check_output',
+                        return_value=_LIST_RESP) as mock_output:
+            self.r.reconcile_tags(self.data, False)
+            mock_output.assert_any_call([_GCLOUD_CONFIG], shell=True)
+            mock_output.assert_any_call([_GCLOUD_LIST], shell=True)
+            mock_output.assert_any_call([self._gcloudAdd(_DIGEST1, _TAG1)],
+                                        shell=True)
+
+    def test_dry_run(self):
+        with mock.patch('subprocess.check_output',
+                        return_value=_LIST_RESP) as mock_output:
+            self.r.reconcile_tags(self.data, True)
+            mock_output.assert_any_call([_GCLOUD_CONFIG], shell=True)
+
+            # These next two lines test identical things, I added the "manual"
+            # check to ensure that my assertNotIn check would theoretically
+            # be looking for the correct thing.
+            mock_output.assert_any_call([_GCLOUD_LIST], shell=True)
+            self.assertIn((([_GCLOUD_LIST],), {'shell': True}),
+                          mock_output.mock_calls)
+
+            self.assertNotIn((([self._gcloudAdd(_DIGEST1, _TAG1)],),
+                              {'shell': True}), mock_output.mock_calls)
+
+    def test_get_existing_tags(self):
+        with mock.patch('subprocess.check_output',
+                        return_value=_LIST_RESP) as mock_output:
+            existing_tags = self.r.get_existing_tags(_REPO)
+            self.assertIn(_TAG1, existing_tags)
+            mock_output.assert_called_once_with([_GCLOUD_LIST], shell=True)
+
+    def test_add_tag(self):
+        with mock.patch('subprocess.check_output',
+                        return_value=_LIST_RESP) as mock_output:
+            self.r.add_tags(_REPO+'@sha256:'+_DIGEST2, _REPO+':'+_TAG2, False)
+            mock_output.assert_called_once_with(
+                [self._gcloudAdd(_DIGEST2, _TAG2)], shell=True)
+
+if __name__ == '__main__':
+    unittest.main()
