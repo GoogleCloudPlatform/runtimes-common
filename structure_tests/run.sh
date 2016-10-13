4ef2bc0acabea15849f5bc096e65cf027532ad23
Nick Kubala <nkubala@google.com>
2016-10-13 16:06:11 -0700

    add support for specifying custom structure test JSON config and verbose flag as build step args

--- structure_tests/run.sh
+++ structure_tests/run.sh
@@ -2,12 +2,50 @@
 
 export DOCKER_API_VERSION="1.21"
 
-IMAGE_NAME=$1
-if [ -z "$1" ]; then
-  echo "Please provide fully qualified path to image under test."
-  exit 1
-fi
+export VERBOSE=0
+while test $# -gt 0; do
+	case "$1" in
+		--image|-i)
+			shift
+			if test $# -gt 0; then
+				export IMAGE_NAME=$1
+			else
+				echo "Please provide fully qualified path to image under test."
+				exit 1
+			fi
+			shift
+			;;
+		--verbose|-v)
+			export VERBOSE=1
+			shift
+			;;
+		--config|-c)
+			shift
+			if test $# -eq 0; then
+				echo "Please provide fully qualified path to config file."
+				exit 1
+			else
+				export CONFIG=$1
+			fi
+			shift
+			;;
+		*)
+			shift
+			;;
+	esac
+done
 
 cp /test/* /workspace/
 
-docker run --privileged=true -v /workspace:/workspace "$IMAGE_NAME" /bin/sh -c "/workspace/structure_test"
+export CMD_STRING="/workspace/structure_test"
+echo "config: $CONFIG"
+if [ -n "$CONFIG" ]; then
+	CMD_STRING=$CMD_STRING" --config $CONFIG"
+fi
+
+if [ $VERBOSE -eq 1 ]; then
+	CMD_STRING=$CMD_STRING" -v"
+echo "$CMD_STRING"
+fi
+
+docker run --privileged=true -v /workspace:/workspace "$IMAGE_NAME" /bin/sh -c "$CMD_STRING"
