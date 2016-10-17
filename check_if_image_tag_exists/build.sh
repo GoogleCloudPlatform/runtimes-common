e92725bcaf138dad9ef25acd0217e6530fabb920
Nick Kubala <nkubala@google.com>
2016-10-17 11:48:49 -0700

    shellcheck

--- check_if_image_tag_exists/build.sh
+++ check_if_image_tag_exists/build.sh
@@ -7,9 +7,9 @@ set -e
 export IMAGE=$1
 export VERSION=$2
 
-if [ -z $IMAGE ]; then
+if [ -z "$IMAGE" ]; then
   usage
-else if [ -z $VERSION ]; then
+elif [ -z "$VERSION" ]; then
 	echo "Defaulting to latest JSON schema version..."
 	export VERSION="latest"
 fi
