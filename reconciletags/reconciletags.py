3f6ed4050abcfa1553aa2a6a4f551c8f2e8826dd
Sharif Elgamal <selgamal@google.com>
2016-10-19 10:20:33 -0700

    adding support for multiple json files (#32)
    * adding support for multiple json files
    
    * use nargs for multiple files
    
    * small changes

--- reconciletags/reconciletags.py
+++ reconciletags/reconciletags.py
@@ -63,17 +63,19 @@ class TagReconciler:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dry-run', dest="dry_run",
+    parser.add_argument('--dry-run', dest='dry_run',
                         action='store_true', default=False)
-    parser.add_argument('file',
-                        help='The file to run the reconciler on')
+    parser.add_argument('files',
+                        help='The files to run the reconciler on',
+                        nargs='+')
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG)
     r = TagReconciler()
-    logging.debug('---Processing {0}---'.format(args.file))
-    with open(args.file) as tag_map:
-        data = json.load(tag_map)
-    r.reconcile_tags(data, args.dry_run)
+    for f in args.files:
+        logging.debug('---Processing {0}---'.format(f))
+        with open(f) as tag_map:
+            data = json.load(tag_map)
+            r.reconcile_tags(data, args.dry_run)
 
 if __name__ == '__main__':
     main()
