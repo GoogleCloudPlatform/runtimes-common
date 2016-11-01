d238d5e514e219235cef8d70e515005abf355e03
Nick Kubala <nkubala@google.com>
2016-11-01 13:23:29 -0700

    use correct structure test image. use random number for tmp dir and cleanup properly. add comments for config

--- structure_tests/ext_run.sh
+++ structure_tests/ext_run.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
+export VERBOSE=0
+export CMD_STRING=""
+export ENTRYPOINT="./test/structure_test"
+export ST_IMAGE="gcr.io/gcp-runtimes/structure_test"
+CONFIG_DIR="/tmp/$RANDOM"
+
+declare -i CONFIG_COUNTER=0
+
+while [ -d "$CONFIG_DIR" ]; do
+	CONFIG_DIR="/tmp/$RANDOM"
+done
+
+mkdir "$CONFIG_DIR"
+
 command -v docker > /dev/null 2>&1 || { echo "Docker is required to run GCP structure tests, but is not installed on this host."; exit 1; }
 
 command docker ps > /dev/null 2>&1 || { echo "Cannot connect to the Docker daemon!"; exit 1; }
 
+cleanup() {
+	if [ -d "$CONFIG_DIR" ]; then
+		rm -rf "$CONFIG_DIR"
+	fi
+}
+
 usage() {
 	echo "Usage: $0 [-i <image>] [-c <config>] [-v] [-e <entrypoint>]"
+	cleanup
 	exit 1
 }
 
-export VERBOSE=0
-export CMD_STRING=""
-export ENTRYPOINT="./test/structure_test"
-export ST_IMAGE="gcr.io/nick-cloudbuild/structure_test"
-
-declare -i CONFIG_COUNTER=0
-
-mkdir /tmp/st_configs
 
 while test $# -gt 0; do
 	case "$1" in
@@ -47,7 +60,17 @@ while test $# -gt 0; do
 			if test $# -eq 0; then
 				usage
 			else
-				cp "$1" /tmp/st_configs/cfg_$CONFIG_COUNTER.json
+				if [ ! -f "$1" ]; then
+					echo "$1 is not a valid file."
+					cleanup
+					exit 1
+				fi
+				# structure tests allow specifying any number of json configs,
+				# which can live anywhere on the host file system. to simplify
+				# the docker volume mount, we copy all of these configs into
+				# a /tmp directory and mount this single directory into the
+				# test image. this directory is cleaned up after testing.
+				cp "$1" "$CONFIG_DIR"/cfg_$CONFIG_COUNTER.json
 				CMD_STRING=$CMD_STRING" --config /cfg/cfg_$CONFIG_COUNTER.json"
 				CONFIG_COUNTER+=1
 			fi
@@ -71,11 +94,13 @@ docker pull "$ST_IMAGE"
 
 ST_CONTAINER=$(docker run -d --entrypoint="/bin/sh" --name st_container "$ST_IMAGE")
 if [ -z "$ST_CONTAINER" ]; then
+	cleanup
 	exit 1
 fi
 
-TEST_CONTAINER=$(docker create --entrypoint="$ENTRYPOINT" --volumes-from st_container -v /tmp/st_configs:/cfg "$IMAGE_NAME" $CMD_STRING)
+TEST_CONTAINER=$(docker create --entrypoint="$ENTRYPOINT" --volumes-from st_container -v "$CONFIG_DIR":/cfg "$IMAGE_NAME" $CMD_STRING)
 if [ -z "$TEST_CONTAINER" ]; then
+	cleanup
 	exit 1
 fi
 
@@ -88,4 +113,4 @@ done
 
 docker rm "$TEST_CONTAINER" > /dev/null 2>&1
 docker rm "$ST_CONTAINER" > /dev/null 2>&1
-rm -rf /tmp/st_configs
+cleanup
