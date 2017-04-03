3a6effb80adb0652e2f842d220ad3135801053ef
Nick Kubala <nkubala@google.com>
2017-04-03 15:58:19 -0700

    flake

--- apt_builder/install_packages.py
+++ apt_builder/install_packages.py
@@ -16,13 +16,15 @@ APT_INSTALL = '''
     {package_list} \\'''
 
 REMOVE_TOOLS = '''
-    && apt-get remove -y --force-yes software-properties-common python-software-properties \\
+    && apt-get remove -y --force-yes software-properties-common \\
+    python-software-properties \\
     && apt-get autoremove -y --force-yes \\
     && apt-get clean -y --force-yes
 
 '''
 
 DOCKERFILE_LOCATION = '/workspace/Dockerfile'
+
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
@@ -47,14 +49,19 @@ def install_packages(yaml_file, image):
         config = yaml.round_trip_load(app_yaml)
         apt_packages = config['packages']['apt']
         for ppa in apt_packages['ppas']:
+            logging.info('Attempting to add PPA repository: {0}'
+                         .format(ppa))
             PPA_STRING = PPA_ADD.format(ppa=ppa)
-            INSTALL_STRING = INSTALL_STRING + PPA_STRING
+            INSTALL_STRING += PPA_STRING
 
         package_list = apt_packages['packages']
         if package_list:
-            APT_STRING = APT_INSTALL.format(package_list=' \\\n\t'.join(package_list))
-            INSTALL_STRING = INSTALL_STRING + APT_STRING
-    INSTALL_STRING = INSTALL_STRING + REMOVE_TOOLS
+            logging.info('Attempting to install the following packages:')
+            logging.info(package_list)
+            PACKAGE_STR = ' \\\n\t'.join(package_list)
+            APT_STRING = APT_INSTALL.format(package_list=PACKAGE_STR)
+            INSTALL_STRING += APT_STRING
+    INSTALL_STRING += REMOVE_TOOLS
 
     with open(DOCKERFILE_LOCATION, 'w') as dockerfile:
         dockerfile.write(INSTALL_STRING)
