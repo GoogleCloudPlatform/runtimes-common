644a3bde857f7bd1697880cf2456bee7ea46adea
Nick Kubala <nkubala@google.com>
2017-04-03 15:43:26 -0700

    apt-builder now works as intermediate cloudbuild step

--- apt_builder/install_packages.py
+++ apt_builder/install_packages.py
@@ -1,71 +1,64 @@
 #!/usr/bin/python
 
 import argparse
+import logging
 from ruamel import yaml
 
-INSTALL_TOOLS = 'RUN apt-get update && apt-get install -y --force-yes software-properties-common python-software-properties\n'
-PPA_ADD = 'RUN add-apt-repository -y {ppa}\n'
-APT_INSTALL = 'RUN apt-get install -y --force-yes {package_list}\n'
-REMOVE_TOOLS = 'RUN apt-get remove software-properties-common python-software-properties\n && apt-get autoremove && apt-get clean\n'
+INSTALL_TOOLS = '''FROM {0}
+RUN apt-get update && apt-get install -y --force-yes \\
+    software-properties-common python-software-properties \\'''
+
+PPA_ADD = '''
+    && add-apt-repository -y {ppa} \\'''
+
+APT_INSTALL = '''
+    && apt-get install -y --force-yes \\
+    {package_list} \\'''
+
+REMOVE_TOOLS = '''
+    && apt-get remove -y --force-yes software-properties-common python-software-properties \\
+    && apt-get autoremove -y --force-yes \\
+    && apt-get clean -y --force-yes
+
+'''
 
 DOCKERFILE_LOCATION = '/workspace/Dockerfile'
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--yaml', '-y',
-						help='app.yaml')
+    logging.getLogger().setLevel(logging.INFO)
 
-	parser.add_argument('--dockerfile', '-d',
-						help='dockerfile', default=DOCKERFILE_LOCATION)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--yaml', '-y',
+                        help='app.yaml')
 
-	args = parser.parse_args()
+    parser.add_argument('--image', '-i',
+                        default='INTERMEDIATE',
+                        help='intermediate image')
 
-	install_packages(args.yaml, args.dockerfile)
+    args = parser.parse_args()
 
-
-def install_packages(yaml_file, dockerfile_location):
-	with open(dockerfile_location, 'r+') as dockerfile:
-		lines = dockerfile.readlines()
-		dockerfile.seek(0)
-		for line in lines:
-			print line
-			if 'ENTRYPOINT' in line:
-				print INSTALL_TOOLS
-				dockerfile.write(INSTALL_TOOLS)
-
-				with open(yaml_file, 'r') as app_yaml:
-					config = yaml.round_trip_load(app_yaml)
-			    	apt_packages = config['packages']['apt']
-			    	for ppa in apt_packages['ppas']:
-			    		# print ppa
-			    		dockerfile.write(PPA_ADD.format(ppa=ppa))
-
-			    	package_list = apt_packages['packages']
-			    	if package_list:
-			    		# print package_list
-			    		# print ' '.join(package_list)
-						dockerfile.write(APT_INSTALL.format(package_list=' '.join(package_list)))
-				dockerfile.write(REMOVE_TOOLS)
-				dockerfile.write('\n')
-			dockerfile.write(line)
-		dockerfile.truncate()
+    install_packages(args.yaml, args.image)
 
 
-		# dockerfile.write(INSTALL_TOOLS)
+def install_packages(yaml_file, image):
+    INSTALL_STRING = INSTALL_TOOLS.format(image)
 
-		# with open(yaml_file, 'r') as app_yaml:
-		# 	config = yaml.round_trip_load(app_yaml)
-	 #    	apt_packages = config['packages']['apt']
-	 #    	for ppa in apt_packages['ppas']:
-	 #    		# print ppa
-	 #    		dockerfile.write(PPA_ADD.format(ppa=ppa))
+    with open(yaml_file, 'r') as app_yaml:
+        config = yaml.round_trip_load(app_yaml)
+        apt_packages = config['packages']['apt']
+        for ppa in apt_packages['ppas']:
+            PPA_STRING = PPA_ADD.format(ppa=ppa)
+            INSTALL_STRING = INSTALL_STRING + PPA_STRING
 
-	 #    	package_list = apt_packages['packages']
-	 #    	if package_list:
-	 #    		# print package_list
-	 #    		# print ' '.join(package_list)
-		# 		dockerfile.write(APT_INSTALL.format(package_list=' '.join(package_list)))
+        package_list = apt_packages['packages']
+        if package_list:
+            APT_STRING = APT_INSTALL.format(package_list=' \\\n\t'.join(package_list))
+            INSTALL_STRING = INSTALL_STRING + APT_STRING
+    INSTALL_STRING = INSTALL_STRING + REMOVE_TOOLS
+
+    with open(DOCKERFILE_LOCATION, 'w') as dockerfile:
+        dockerfile.write(INSTALL_STRING)
 
 
 if __name__ == '__main__':
-	main()
+    main()
