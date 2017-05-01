d7a1009e3c18433e5133ad3294c8b05b3600c6fb
sharifelgamal <selgamal@google.com>
2017-05-01 15:48:35 -0700

    adding check to test bucket permissions

---
+++ reconciletags/bucket_acl_test.py
@@ -0,0 +1,56 @@
+"""Bucket ACL test.
+
+Looks at the GCS buckets where all GCR container information is stored and
+makes sure they're all world readable."""
+
+import datetime
+import glob
+import json
+import logging
+import os
+import subprocess
+import unittest
+
+
+class BucketAclTest(unittest.TestCase):
+
+    def _get_acls(self, bucket):
+        acls = json.loads(
+                  subprocess.check_output(['gsutil', 'acl',
+                                           'get', bucket]))
+        return acls
+
+    def _get_bucket_name(self, repo, mirror):
+        bucket_name = 'artifacts.{0}.appspot.com'.format(repo)
+        if mirror:
+            bucket_name = '{0}.{1}'.format(mirror, bucket_name)
+        return 'gs://{0}'.format(bucket_name)
+
+    def test_bucket_acls(self):
+        repos = ['gcp-runtimes', 'google-appengine']
+        mirrors = ['', 'asia', 'eu', 'us']
+        bad_buckets = []
+        for repo in repos:
+            for mirror in mirrors:
+                # Construct bucket name
+                bucket_name = self._get_bucket_name(repo, mirror)
+                # Grab acls json
+                acls = self._get_acls(bucket_name)
+                # check that entity: allUsers and role: READER are somewhere in the
+                # json. if not, add to list of failing buckets
+                valid = False
+                for acl in acls:
+                    if acl['entity'] == 'allUsers':
+                        if acl['role'] == 'READER':
+                            valid = True
+                            break
+                if not valid:
+                    bad_buckets.append(bucket_name)
+
+        if len(bad_buckets) > 0:
+            self.fail('The following buckets have incorrect ACLs:'
+                      + str(bad_buckets))
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
