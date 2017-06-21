dfcaa6f1f463b898622c0ac1a272406abd8a6bd3
Abby Tisdale <atisdale@google.com>
2017-06-21 10:30:00 -0700

    Tar extraction, including nested tar, with testing

---
+++ iDiff/util/tar_utils.go
@@ -0,0 +1,100 @@
+package tarUtil
+
+import (
+	"archive/tar"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Dir stores a representaiton of a file directory.
+type Dir struct {
+	Name  string
+	Files []string
+	Dirs  []Dir
+}
+
+// UnTar takes in a path to a tar file and writes the untarred version to the provided target.
+// Only untars one level, does not untar nested tars.
+func UnTar(filename string, path string) error {
+	if _, ok := os.Stat(path); ok != nil {
+		os.MkdirAll(path, 0777)
+
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	tr := tar.NewReader(file)
+
+	for {
+		header, err := tr.Next()
+		if err == io.EOF {
+			// end of tar archive
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		target := filepath.Join(path, header.Name)
+		mode := header.FileInfo().Mode()
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, mode); err != nil {
+					return err
+				}
+				continue
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+
+			currFile, err := os.OpenFile(target, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, mode)
+			if err != nil {
+				return err
+			}
+			defer currFile.Close()
+			_, err = io.Copy(currFile, tr)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isTar(path string) bool {
+	return filepath.Ext(path) == ".tar"
+}
+
+// ExtractTar extracts the tar and any nested tar at the given path.
+// After execution the original tar file is removed and the untarred version is in it place.
+func ExtractTar(path string) error {
+	removeTar := false
+
+	var untarWalkFn func(path string, info os.FileInfo, err error) error
+
+	untarWalkFn = func(path string, info os.FileInfo, err error) error {
+		if isTar(path) {
+			target := strings.TrimSuffix(path, filepath.Ext(path))
+			UnTar(path, target)
+			if removeTar {
+				os.Remove(path)
+			}
+			// remove nested tar files that get copied but not the original tar passed
+			removeTar = true
+			filepath.Walk(target, untarWalkFn)
+		}
+		return nil
+	}
+
+	return filepath.Walk(path, untarWalkFn)
+}
---
+++ iDiff/util/tar_utils_test.go
@@ -0,0 +1,118 @@
+package tarUtil
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestUnTar(t *testing.T) {
+	testCases := []struct {
+		descrip  string
+		tarPath  string
+		target   string
+		expected string
+		err      bool
+	}{
+		{
+			descrip:  "Tar with files",
+			tarPath:  "testTars/la-croix1.tar",
+			target:   "testTars/la-croix1",
+			expected: "testTars/la-croix1-actual",
+		},
+		{
+			descrip:  "Tar with folders with files",
+			tarPath:  "testTars/la-croix2.tar",
+			target:   "testTars/la-croix2",
+			expected: "testTars/la-croix2-actual",
+		},
+		{
+			descrip:  "Tar with folders with files and a tar file",
+			tarPath:  "testTars/la-croix3.tar",
+			target:   "testTars/la-croix3",
+			expected: "testTars/la-croix3-actual",
+		},
+	}
+	for _, test := range testCases {
+		err := UnTar(test.tarPath, test.target)
+		if err != nil && !test.err {
+			t.Errorf("Got unexpected error: %s", err)
+		}
+		if err == nil && test.err {
+			t.Errorf("Expected error but got none: %s", err)
+		}
+		if !dirEquals(test.expected, test.target) || !dirEquals(test.target, test.expected) {
+			t.Errorf("Directory created not correct structure.")
+		}
+		os.RemoveAll(test.target)
+	}
+
+}
+
+func TestIsTar(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected bool
+	}{
+		{input: "/testTar/la-croix1.tar", expected: true},
+		{input: "/testTar/la-croix1-actual", expected: false},
+	}
+	for _, test := range testCases {
+		actual := isTar(test.input)
+		if test.expected != actual {
+			t.Errorf("Expected: %t but got: %t", test.expected, actual)
+		}
+	}
+}
+
+func TestExtractTar(t *testing.T) {
+	tarPath := "testTars/la-croix3.tar"
+	target := "testTars/la-croix3"
+	expected := "testTars/la-croix3-full"
+	err := ExtractTar(tarPath)
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+	if !dirEquals(expected, target) || !dirEquals(target, expected) {
+		t.Errorf("Directory created not correct structure.")
+	}
+	os.RemoveAll(target)
+
+}
+
+func dirEquals(actual string, path string) bool {
+	files1, _ := ioutil.ReadDir(actual)
+
+	for _, file := range files1 {
+		newActualPath := filepath.Join(actual, file.Name())
+		newExpectedPath := filepath.Join(path, file.Name())
+		fstat, ok := os.Stat(newExpectedPath)
+		if ok != nil {
+			return false
+		}
+
+		if file.IsDir() && !dirEquals(newActualPath, newExpectedPath) {
+			return false
+		}
+
+		if fstat.Name() != file.Name() {
+			return false
+		}
+		if fstat.Size() != file.Size() {
+			return false
+		}
+		if filepath.Ext(file.Name()) == ".tar" {
+			continue
+		}
+
+		content1, _ := ioutil.ReadFile(newActualPath)
+		content2, _ := ioutil.ReadFile(newExpectedPath)
+
+		if 0 != bytes.Compare(content1, content2) {
+			return false
+		}
+	}
+	return true
+}
