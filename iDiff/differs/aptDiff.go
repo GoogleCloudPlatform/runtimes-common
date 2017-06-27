66a9905b16924f804ba65efc38ea13fccd3165cd
Abby Tisdale <atisdale@google.com>
2017-06-27 13:33:49 -0700

    Refactor into smaller pieces before migrating common functionality to util.

--- iDiff/differs/aptDiff.go
+++ iDiff/differs/aptDiff.go
@@ -7,28 +7,36 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
+
+	"github.com/golang/glog"
 )
 
 type PackageDiff struct {
-	Image1      string
-	Packages1   []string
-	Image2      string
-	Packages2   []string
-	VersionDiff []VDiff
+	Image1    string
+	Packages1 []string
+	Image2    string
+	Packages2 []string
+	InfoDiff  []Info
 }
 
-type VDiff struct {
-	Package  string
-	Version1 string
-	Version2 string
+type Info struct {
+	Package string
+	Info1   PackageInfo
+	Info2   PackageInfo
+}
+
+type PackageInfo struct {
+	Version string
+	Size    string
 }
 
 func output(diff PackageDiff) error {
 	const master = `Packages found only in {{.Image1}}:{{block "list" .Packages1}}{{"\n"}}{{range .}}{{println "-" .}}{{end}}{{end}}
 Packages found only in {{.Image2}}:{{block "list2" .Packages2}}{{"\n"}}{{range .}}{{println "-" .}}{{end}}{{end}}
-Version differences:{{"\n"}}	(Package: {{.Image1}}{{"\t\t"}}{{.Image2}}){{range .VersionDiff}}
-	{{.Package}}: {{.Version1}}	{{.Version2}}
+Version differences:{{"\n"}}	(Package:	{{.Image1}}{{"\t\t"}}{{.Image2}}){{range .InfoDiff}}
+	{{.Package}}:	{{.Info1.Version}}	{{.Info2.Version}}
 	{{end}}`
 
 	funcs := template.FuncMap{"join": strings.Join}
@@ -62,72 +70,108 @@ func AptDiff(img1, img2 string) (string, error) {
 	return "", nil
 }
 
-func diffMaps(map1, map2 map[string]string) PackageDiff {
+func (pi PackageInfo) string() string {
+	return pi.Version
+}
+
+func diffMaps(map1, map2 map[string]PackageInfo) PackageDiff {
 	diff1 := []string{}
 	diff2 := []string{}
-	versionDiff := []VDiff{}
+	infoDiff := []Info{}
 	for key1, value1 := range map1 {
 		value2, ok := map2[key1]
 		if !ok {
-			diff1 = append(diff1, key1+":"+value1)
-		} else if value2 != value1 {
-			versionDiff = append(versionDiff, VDiff{key1, value1, value2})
+			diff1 = append(diff1, key1+":"+value1.string())
+		} else if !reflect.DeepEqual(value2, value1) {
+			infoDiff = append(infoDiff, Info{key1, value1, value2})
 			delete(map2, key1)
 		} else {
 			delete(map2, key1)
 		}
 	}
 	for key2, value2 := range map2 {
-		diff2 = append(diff2, key2+":"+value2)
+		diff2 = append(diff2, key2+":"+value2.string())
 	}
-	diff := PackageDiff{Packages1: diff1, Packages2: diff2, VersionDiff: versionDiff}
+	diff := PackageDiff{Packages1: diff1, Packages2: diff2, InfoDiff: infoDiff}
 	return diff
 }
 
-func getPackages(path string) (map[string]string, error) {
-	packages := make(map[string]string)
-
-	var layerStems []string
-
+// TODO move this to the general differ and modify to only return changed directories
+func getLayers(path string) ([]os.FileInfo, error) {
 	layers, err := ioutil.ReadDir(path)
+	if err != nil {
+		return []os.FileInfo{}, err
+	}
+	return layers, nil
+}
+
+// TODO move to general differ
+func buildLayerTargets(path, target string) ([]string, error) {
+	var layerStems []string
+	layers, err := getLayers(path)
+	if err != nil {
+		return layerStems, err
+	}
+	for _, layer := range layers {
+		layerStems = append(layerStems, filepath.Join(path, layer.Name(), target))
+	}
+	return layerStems, nil
+
+}
+
+func getPackages(path string) (map[string]PackageInfo, error) {
+	packages := make(map[string]PackageInfo)
+	layerStems, err := buildLayerTargets(path, "layer/var/lib/dpkg/status")
 	if err != nil {
 		return packages, err
 	}
-	for _, layer := range layers {
-		layerStems = append(layerStems, filepath.Join(path, layer.Name(), "layer/var/lib/dpkg/status"))
-	}
-
 	for _, statusFile := range layerStems {
-		if _, err := os.Stat(statusFile); err == nil {
-			if file, err := os.Open(statusFile); err == nil {
-				// make sure it gets closed
-				defer file.Close()
-
-				var currPackage string
-				// create a new scanner and read the file line by line
-				scanner := bufio.NewScanner(file)
-				for scanner.Scan() {
-					line := strings.Split(scanner.Text(), ": ")
-					if len(line) == 2 {
-						key := line[0]
-						value := line[1]
-						if key == "Package" {
-							currPackage = value
-						}
-						if key == "Version" {
-							packages[currPackage] = value
-						}
-					}
-
-				}
-
-			} else {
-				return packages, err
-			}
-		} else {
+		if _, err := os.Stat(statusFile); err != nil {
 			// status file does not exist in this layer
 			continue
 		}
+		if file, err := os.Open(statusFile); err == nil {
+			// make sure it gets closed
+			defer file.Close()
+
+			// create a new scanner and read the file line by line
+			scanner := bufio.NewScanner(file)
+			var currPackage string
+			for scanner.Scan() {
+				currPackage = parseLine(scanner.Text(), currPackage, packages)
+			}
+		} else {
+			return packages, err
+		}
 	}
 	return packages, nil
+}
+
+func parseLine(text string, currPackage string, packages map[string]PackageInfo) string {
+	line := strings.Split(text, ": ")
+	if len(line) == 2 {
+		key := line[0]
+		value := line[1]
+
+		switch key {
+		case "Package":
+			return value
+		case "Version":
+			if packages[currPackage].Version != "" {
+				glog.Warningln("Multiple versions of same package detected.  Diffing such multi-versioning not yet supported.")
+				return currPackage
+			}
+			tempPackage := PackageInfo{Version: value}
+			packages[currPackage] = tempPackage
+			return currPackage
+
+		case "Installed-Size":
+			tempPackage := PackageInfo{Size: value}
+			packages[currPackage] = tempPackage
+			return currPackage
+		default:
+			return currPackage
+		}
+	}
+	return currPackage
 }
