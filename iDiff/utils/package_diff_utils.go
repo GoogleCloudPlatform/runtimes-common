72c524efd163ca966fcddbc209f96f0dd2399317
Abby Tisdale <atisdale@google.com>
2017-07-07 09:43:25 -0700

    Starting multiversion handling

--- iDiff/utils/package_diff_utils.go
+++ iDiff/utils/package_diff_utils.go
@@ -6,12 +6,21 @@ import (
 	"reflect"
 )
 
+type MultiVersionPackageDiff struct {
+	Image1    string
+	Packages1 map[string]map[string]PackageInfo
+	Image2    string
+	Packages2 map[string]map[string]PackageInfo
+	InfoDiff  []Info
+}
+
+
 // PackageDiff stores the difference information between two images.
 type PackageDiff struct {
 	Image1    string
-	Packages1 []string
+	Packages1 map[string]PackageInfo
 	Image2    string
-	Packages2 []string
+	Packages2 map[string]PackageInfo
 	InfoDiff  []Info
 }
 
@@ -26,28 +35,48 @@ type Info struct {
 type PackageInfo struct {
 	Version string
 	Size    string
+	Path 	string
+}
+
+
+func multiVersionDiff(infoDiff []Info{}, key1 string, value1, value2 map[string]PackageInfo{}) {
+	
 }
 
 // DiffMaps determines the differences between maps of package names to PackageInfo structs
 // The return struct includes a list of packages only in the first map, a list of packages only in
 // the second map, and a list of packages which differed only in their PackageInfo (version, size, etc.)
-func DiffMaps(map1, map2 map[string]PackageInfo) PackageDiff {
-	diff1 := []string{}
-	diff2 := []string{}
+func DiffMaps(map1, map2 interface{}) interface{} {
+	multiVersion := false
+	if map1.(type) == map[string]map[string]PackageInfo{} {
+		multiVersion = true
+	}
+
+	if map1.(type) != map2.(type) {
+		glog.Error("Package maps were of different types.")
+	}
+
+	diff1 := map1.(type){}
+	diff2 := map1.(type){}
 	infoDiff := []Info{}
 	for key1, value1 := range map1 {
 		value2, ok := map2[key1]
 		if !ok {
-			diff1 = append(diff1, key1+":"+value1.string())
+			diff1[key1] = value1
 		} else if !reflect.DeepEqual(value2, value1) {
-			infoDiff = append(infoDiff, Info{key1, value1, value2})
-			delete(map2, key1)
+			if multiVersion {
+				multiVersionDiff(infoDiff, key1, value1, value2)
+				// infoDiff = append(infoDiff, Info{key1, value1, value2})
+			} else {
+				infoDiff = append(infoDiff, Info{key1, value1, value2})
+				delete(map2, key1)
+			}
 		} else {
 			delete(map2, key1)
 		}
 	}
 	for key2, value2 := range map2 {
-		diff2 = append(diff2, key2+":"+value2.string())
+		diff2[key2] = value2
 	}
 	diff := PackageDiff{Packages1: diff1, Packages2: diff2, InfoDiff: infoDiff}
 	return diff
