93f30417098d5a19b263fab10ddac7c98c31e259
Colette Torres <colettet@google.com>
2017-07-10 20:25:04 -0400

    Pip Differ (#221)
    * Added PythonDiff
    
    * Made PipDiff use JSON format

--- iDiff/differs/differs.go
+++ iDiff/differs/differs.go
@@ -12,6 +12,7 @@ var diffs = map[string]func(string, string, bool) (string, error){
 	"hist": History,
 	"dir":  Package,
 	"apt":  AptDiff,
+	"pip":  PipDiff,
 }
 
 func Diff(arg1, arg2, differ string, json bool) (string, error) {
