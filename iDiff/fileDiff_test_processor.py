43f922f7a4e4c7c17fc3c85b3d81726ad80535a1
Colette <colettet@google.com>
2017-07-18 10:21:01 -0700

    Made python changes

--- iDiff/fileDiff_test_processor.py
+++ iDiff/fileDiff_test_processor.py
@@ -4,15 +4,14 @@ import sys
 
 
 def _process_test_diff(file_path):
-    f = open(file_path)
-    diff = json.load(f)
-    f.close()
+    with open(file_path) as f:
+        diff = json.load(f)
 
     diff["Adds"] = _trim_file_names(diff["Adds"])
     diff["Dels"] = _trim_file_names(diff["Dels"])
 
-    f = open(file_path, 'w')
-    json.dump(diff, f, indent=4)
+    with open(file_path, 'w') as f:
+        json.dump(diff, f, indent=4)
 
 
 def _trim_file_names(files):
@@ -27,7 +26,7 @@ def _trim_layer_hash(filename):
     hash_match = re.match(r'^([a-z|0-9]{64})/', filename)
     if hash_match:
         hash = hash_match.group(1)
-        return filename[len(hash):]
+        return re.sub(hash, "", filename)
     return filename
 
 
