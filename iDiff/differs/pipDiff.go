b576538fff69495fa8a64562244bd869fb20af38
Abby Tisdale <atisdale@google.com>
2017-08-03 14:51:46 -0700

    Fixing merge errors

--- iDiff/differs/pipDiff.go
+++ iDiff/differs/pipDiff.go
@@ -36,21 +36,20 @@ func getPythonVersion(pathToLayer string) ([]string, error) {
 	return matches, nil
 }
 
-
 func (d PipDiffer) getPackages(path string) (map[string]map[string]utils.PackageInfo, error) {
 	packages := make(map[string]map[string]utils.PackageInfo)
 
 	pythonVersions, err := getPythonVersion(path)
 	if err != nil {
 		// layer doesn't have a Python version installed
-		return packages
+		return packages, nil
 	}
 	for _, pyVersion := range pythonVersions {
 		packagesPath := filepath.Join(path, "usr/local/lib", pyVersion, "site-packages")
 		contents, err := ioutil.ReadDir(packagesPath)
 		if err != nil {
 			// layer's Python folder doesn't have a site-packages folder
-			return packages
+			return packages, nil
 		}
 
 		for _, c := range contents {
