4914d263a92ccffd852f3b9b7f60164baafe7d22
Nick Kubala <nkubala@google.com>
2017-08-18 13:16:39 -0700

    update other hidden files

--- .buildifier.sh
+++ .buildifier.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2181
 # shellcheck disable=SC2046
-files=$(buildifier -mode=check $(find . -name 'BUILD*' -o -name '*.bzl' -type f | grep -v iDiff/vendor))
+files=$(buildifier -mode=check $(find . -name 'BUILD*' -o -name '*.bzl' -type f))
 if [ $? -ne 0 ]; then
   exit 1
 fi
