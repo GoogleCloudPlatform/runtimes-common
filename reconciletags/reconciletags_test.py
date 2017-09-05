0490247e25f3ec62df236572e627e1371f30fe59
Priya Wadhwa <priyawadhwa@google.com>
2017-09-05 15:07:19 -0700

    cleaned up code

--- reconciletags/reconciletags_test.py
+++ reconciletags/reconciletags_test.py
@@ -17,11 +17,11 @@
 Unit tests for reconcile-tags.py.
 """
 import unittest
+from containerregistry.client.v2_2 import docker_image
+from containerregistry.client.v2_2 import docker_session
 import mock
 from mock import patch
 import reconciletags
-from containerregistry.client.v2_2 import docker_image
-from containerregistry.client.v2_2 import docker_session
 
 _REGISTRY = 'gcr.io'
 _REPO = 'foobar/baz'
@@ -44,15 +44,16 @@ _LIST_RESP = """
 ]
 """
 
-_EXISTING_TAGS = "Existing Tags: {0}".format([_TAG1])
-_TAGGING_DRY_RUN = "Would have tagged {0} with {1}".format(
-                _FULL_REPO+'@sha256:'+_DIGEST1, _FULL_REPO+":"+_TAG1)
+_EXISTING_TAGS = 'Existing Tags: {0}'.format([_TAG1])
+_TAGGING_DRY_RUN = 'Would have tagged {0} with {1}'.format(
+    _FULL_REPO+'@sha256:'+_DIGEST1, _FULL_REPO+":"+_TAG1)
+
 
 class ReconcileTagsTest(unittest.TestCase):
 
     def _tagging(self, digest, tag):
-        return "Tagging {0} with {1}".format(
-                _FULL_REPO+'@sha256:'+digest, _FULL_REPO+":"+tag)
+        return 'Tagging {0} with {1}'.format(
+            _FULL_REPO+'@sha256:'+digest, _FULL_REPO+':'+tag)
 
     def setUp(self):
         self.r = reconciletags.TagReconciler()
@@ -61,7 +62,7 @@ class ReconcileTagsTest(unittest.TestCase):
                        'additional_registries': [],
                        'repository': _REPO,
                        'images': [{'digest': _DIGEST1, 'tag': _TAG1}]}]}
-    
+
     @patch('containerregistry.client.v2_2.docker_session.Push')
     @patch('containerregistry.client.v2_2.docker_image.FromRegistry')
     def test_reconcile_tags(self, mock_from_registry, mock_push):
@@ -73,36 +74,34 @@ class ReconcileTagsTest(unittest.TestCase):
         mock_from_registry.return_value = mock_img
         mock_push.return_value = docker_session.Push()
 
-        
         with mock.patch('reconciletags.logging.debug') as mock_output:
 
             self.r.reconcile_tags(self.data, False)
-            logging_debug_ouput = [call[1][0] for call in mock_output.mock_calls]
+            logging_debug_output = [call[1][0] for call in mock_output.mock_calls]
 
             assert mock_from_registry.called
             assert mock_push.called
 
-            self.assertIn(self._tagging(_DIGEST1, _TAG1), logging_debug_ouput)
-            self.assertIn(_EXISTING_TAGS, logging_debug_ouput)
-    
+            self.assertIn(self._tagging(_DIGEST1, _TAG1), logging_debug_output)
+            self.assertIn(_EXISTING_TAGS, logging_debug_output)
+
     @patch('containerregistry.client.v2_2.docker_session.Push')
     @patch('containerregistry.client.v2_2.docker_image.FromRegistry')
     def test_dry_run(self, mock_from_registry, mock_push):
         mock_from_registry.return_value = docker_image.FromRegistry()
         mock_push.return_value = docker_session.Push()
 
-        
         with mock.patch('reconciletags.logging.debug') as mock_output:
 
             self.r.reconcile_tags(self.data, True)
-            logging_debug_ouput = [call[1][0] for call in mock_output.mock_calls]
+            logging_debug_output = [call[1][0] for call in mock_output.mock_calls]
 
             assert mock_from_registry.called
             assert mock_push.called
 
-            self.assertNotIn(self._tagging(_DIGEST1, _TAG1), logging_debug_ouput)
-            self.assertIn(_TAGGING_DRY_RUN, logging_debug_ouput)
-    
+            self.assertNotIn(self._tagging(_DIGEST1, _TAG1), logging_debug_output)
+            self.assertIn(_TAGGING_DRY_RUN, logging_debug_output)
+
     @patch('containerregistry.client.v2_2.docker_image.FromRegistry')
     def test_get_existing_tags(self, mock_from_registry):
 
@@ -124,18 +123,17 @@ class ReconcileTagsTest(unittest.TestCase):
         mock_from_registry.return_value = docker_image.FromRegistry()
         mock_push.return_value = docker_session.Push()
 
-        
         with mock.patch('reconciletags.logging.debug') as mock_output:
 
             self.r.add_tags(_FULL_REPO+'@sha256:'+_DIGEST2,
                             _FULL_REPO+':'+_TAG2, False)           
-            logging_debug_ouput = [call[1][0] for call in mock_output.mock_calls]
+            logging_debug_output = [call[1][0] for call in mock_output.mock_calls]
 
             assert mock_from_registry.called
             assert mock_push.called
-            
-            self.assertIn(self._tagging(_DIGEST2, _TAG2), logging_debug_ouput) 
-    
+
+            self.assertIn(self._tagging(_DIGEST2, _TAG2), logging_debug_output) 
+
 
 if __name__ == '__main__':
     unittest.main()
