fd9f9a2fba5403469b1dfdbd12c8ee6152654420
Priya Wadhwa <priyawadhwa@google.com>
2017-10-16 11:40:54 -0700

    Updated travis to run main node test

--- testing/lib/mock_from_registry.py
+++ testing/lib/mock_from_registry.py
@@ -20,12 +20,8 @@ class MockFromRegistry(docker_image.FromRegistry):
 
     REGISTRY = mock_registry.MockRegistry()
 
-    def __init__(self, name="", basic_creds=None, transport=None, accepted_mimes=None):
+    def __init__(self, name="", basic_creds=None, transport=None):
         self._name = name
-        self._basic_creds = basic_creds
-        self._transport = transport
-        self._accepted_mimes = accepted_mimes
-        self._response = {}
 
     def setRegistry(self, registry):
         if isinstance(registry, mock_registry.MockRegistry):
@@ -36,9 +32,6 @@ class MockFromRegistry(docker_image.FromRegistry):
 
     def manifests(self):
         return MockFromRegistry.REGISTRY.getManifests(self._name)
-
-    def manifest(self):
-        return self.manifests()
 
     def tags(self):
         return MockFromRegistry.REGISTRY.getTags(self._name)
