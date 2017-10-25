e4ea1769bec0584547879c9854d5fa20caa7efbe
Priya Wadhwa <priyawadhwa@google.com>
2017-10-24 17:34:00 -0700

    kokoro presubmit for ftl integration test

---
+++ ftl/presubmit/presubmit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -ex
+source "$KOKORO_GFILE_DIR/common.sh"
+cd github/runtimes-common
+test_tag="ftl-integration-kokoro-presubmit-$KOKORO_BUILD_NUMBER"
+TAG=$test_tag gcloud container builds submit --config ftl/ftl_node_integration_tests.yaml .
