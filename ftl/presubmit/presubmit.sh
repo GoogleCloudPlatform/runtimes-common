e09c49c095218dd994a7a5e0b4d387b944926a02
Priya Wadhwa <priyawadhwa@google.com>
2017-10-26 09:59:23 -0700

    remove tag

--- ftl/presubmit/presubmit.sh
+++ ftl/presubmit/presubmit.sh
@@ -3,5 +3,4 @@
 set -ex
 source "$KOKORO_GFILE_DIR/common.sh"
 cd github/runtimes-common
-test_tag="ftl-integration-kokoro-presubmit-$KOKORO_BUILD_NUMBER"
-TAG=$test_tag gcloud container builds submit --config ftl/ftl_node_integration_tests.yaml .
+gcloud container builds submit --config ftl/ftl_node_integration_tests.yaml .
