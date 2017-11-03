a1cc5d2f51938633dab0b8f10086ae082a5aeb8b
dlorenc <lorenc.d@gmail.com>
2017-11-03 13:52:38 -0700

    Switch the mock_registry test to use a smaller image. (#428)
    This breaks the remote cache right now, since it has a max size of
    512mb.

--- hack/kokoro-test.sh
+++ hack/kokoro-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# shellcheck source=/dev/null
+source "$KOKORO_GFILE_DIR/common.sh"
+
 # Grab the latest version of shellcheck and add it to PATH
 if [ -f "$KOKORO_GFILE_DIR"/shellcheck-latest.linux ]; then
     sudo cp "$KOKORO_GFILE_DIR"/shellcheck-latest.linux /usr/local/bin/shellcheck
