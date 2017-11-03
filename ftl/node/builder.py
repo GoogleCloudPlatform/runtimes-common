465461574a32fe55ad746699ac5bd53a3bec8ae0
priyawadhwa <priyawadhwa@google.com>
2017-11-03 14:04:33 -0700

    Add ftl no cache flag (#425)

--- ftl/node/builder.py
+++ ftl/node/builder.py
@@ -41,7 +41,7 @@ class Node(builder.JustApp):
         """Override."""
         return self
 
-    def CreatePackageBase(self, base_image, cache):
+    def CreatePackageBase(self, base_image, cache, use_cache=True):
         """Override."""
         # Figure out if we need to override entrypoint.
         # Save the overrides for later to avoid adding an extra layer.
@@ -66,19 +66,26 @@ class Node(builder.JustApp):
                 base_image, tar_gz=None, overrides=overrides)
 
         checksum = hashlib.sha256(descriptor_contents).hexdigest()
-        hit = cache.Get(base_image, _NODE_NAMESPACE, checksum)
-        if hit:
-            logging.info('Found cached dependency layer for %s' % checksum)
-            return hit
+        if use_cache:
+            hit = cache.Get(base_image, _NODE_NAMESPACE, checksum)
+            if hit:
+                logging.info('Found cached dependency layer for %s' % checksum)
+                return hit
+            else:
+                logging.info('No cached dependency layer for %s' % checksum)
         else:
-            logging.info('No cached dependency layer for %s' % checksum)
+            logging.info('Skipping checking cache for dependency layer %s'
+                         % checksum)
 
         layer, sha = self._gen_package_tar(descriptor, descriptor_contents)
 
         with append.Layer(
           base_image, layer, diff_id=sha, overrides=overrides) as dep_image:
-            logging.info('Storing layer %s in cache.', sha)
-            cache.Store(base_image, _NODE_NAMESPACE, checksum, dep_image)
+            if use_cache:
+                logging.info('Storing layer %s in cache.', sha)
+                cache.Store(base_image, _NODE_NAMESPACE, checksum, dep_image)
+            else:
+                logging.info('Skipping storing layer %s in cache.', sha)
             return dep_image
 
     def _gen_package_tar(self, descriptor, descriptor_contents):
