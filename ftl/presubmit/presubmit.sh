cdca41afac81b9ef53ba49214ccf136c98a0020c
dlorenc <dlorenc@google.com>
2017-11-03 13:55:02 -0700

    Merge the two presubmits into one.

--- ftl/presubmit/presubmit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC1090
-set -ex
-source "$KOKORO_GFILE_DIR/common.sh"
-cd github/runtimes-common
-gcloud container builds submit --config ftl/ftl_node_integration_tests.yaml .
