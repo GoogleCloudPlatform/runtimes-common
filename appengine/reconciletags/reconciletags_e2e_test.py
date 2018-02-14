6f866091239aaca477054efba8673bb95cd9800e
Sharif Elgamal <selgamal@google.com>
2018-02-14 14:00:36 -0800

    fixing reconciletags end to end test (#510)

--- appengine/reconciletags/reconciletags_e2e_test.py
+++ appengine/reconciletags/reconciletags_e2e_test.py
@@ -52,7 +52,7 @@ class ReconciletagsE2eTest(unittest.TestCase):
     def _BuildImage(self, full_image_name):
         # create a non-functional but tiny docker image
         subprocess.call(['gcloud', 'container', 'builds', 'submit', '--config',
-                        'reconciletags/e2e_cloudbuild.yaml', '.'])
+                        'appengine/reconciletags/e2e_cloudbuild.yaml', '.'])
         # grab the just created digest
         output = self._ListTags(self._REPO)
         self.assertEqual(len(output), 1)
@@ -95,14 +95,14 @@ class ReconciletagsE2eTest(unittest.TestCase):
             self.r.reconcile_tags(self._TEST_JSON, False)
 
     def testParFile(self):
-        subprocess.call(['./reconciletags/reconciletags.par',
-                        'reconciletags/'+self._FILE_NAME])
+        subprocess.call(['./appengine/reconciletags/reconciletags.par',
+                        'appengine/reconciletags/'+self._FILE_NAME])
         # check list-tags to see if it added the correct tag
         self.checkListTagsOutput()
 
         # run reconciler again and make sure nothing changed
-        subprocess.call(['./reconciletags/reconciletags.par',
-                        'reconciletags/'+self._FILE_NAME])
+        subprocess.call(['./appengine/reconciletags/reconciletags.par',
+                        'appengine/reconciletags/'+self._FILE_NAME])
         self.checkListTagsOutput()
 
 
