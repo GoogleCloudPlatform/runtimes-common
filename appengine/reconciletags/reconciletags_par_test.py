a8d0a4e20fb35a565bcc1e335740ed89e5e2f1a5
sharifelgamal <selgamal@google.com>
2018-02-27 10:50:13 -0800

    flake

--- appengine/reconciletags/reconciletags_par_test.py
+++ appengine/reconciletags/reconciletags_par_test.py
@@ -3,13 +3,16 @@
 import subprocess
 import unittest
 
-class ReconciletagsParTest(unittest.TestCase):
-  _FILE_NAME = 'retagger.json'
 
-  # Just make sure the par file runs on a real image without failing.
-  def testParFile(self):
-      subprocess.call(['./appengine/reconciletags/reconciletags.par',
-                       '--dry-run',
-                       'appengine/reconciletags/'+self._FILE_NAME])
+class ReconciletagsParTest(unittest.TestCase):
+    _FILE_NAME = 'retagger.json'
+
+    # Just make sure the par file runs on a real image without failing.
+    def testParFile(self):
+        subprocess.call(['./appengine/reconciletags/reconciletags.par',
+                         '--dry-run',
+                         'appengine/reconciletags/'+self._FILE_NAME])
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
