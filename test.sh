8510e2cd46ac01d96792235941ac676366cce85a
Jakub Przychodzeń <jprzychodzen@google.com>
2018-03-02 12:54:17 +0100

    Add container tools build to test.sh
    Also, fix docgen README.md

--- test.sh
+++ test.sh
@@ -13,3 +13,9 @@ bazel test --test_output=errors appengine/reconciletags:reconciletags_par_test
 bazel test --test_output=errors ftl/... --deleted_packages=ftl/node/benchmark,ftl/php/benchmark,ftl/python/benchmark,ftl/benchmark
 bazel test --test_output=errors testing/lib:mock_registry_tests
 pushd appengine/runtime_builders && py.test test_manifest.py && popd
+
+# Check building of container related tools
+bazel run //:gazelle
+bazel build docgen/scripts/docgen:docgen
+bazel build versioning/scripts/dockerfiles:dockerfiles
+bazel build versioning/scripts/cloudbuild:cloudbuild
