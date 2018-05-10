a5d62bb7c18a82be6acbb935af06b23d2e3f3ad9
Tejal Desai <tejaldesai@google.com>
2018-05-10 13:12:46 -0700

    Remove secrets

--- hack/kokoro-test.sh
+++ hack/kokoro-test.sh
@@ -63,6 +63,9 @@ pids+=($!)
 gcloud container builds submit --config ftl/integration_tests/ftl_python_error_test.yaml . > python_error.log &
 f_pids+=($!)
 
+bazel test integrationtest/... > integrationtest.log &
+f_pids+=($!)
+
 # Wait for them to finish, and check the exit codes.
 
 failures=0
