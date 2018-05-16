f8e3024555f232bc1977b0bf8cb0969d988ac58b
Tejal Desai <tejaldesai@google.com>
2018-05-16 13:11:42 -0700

    remove action_env and see if tests are green

--- hack/kokoro-test.sh
+++ hack/kokoro-test.sh
@@ -58,7 +58,7 @@ pids+=($!)
 gcloud container builds submit --config ftl/integration_tests/ftl_python_error_test.yaml . > python_error.log &
 f_pids+=($!)
 
-bazel test --test_output=errors integrationtest/tuf/... --action_env GOOGLE_APPLICATION_CREDENTIALS
+bazel test --test_output=errors integrationtest/tuf/...
 
 # Wait for them to finish, and check the exit codes.
 
