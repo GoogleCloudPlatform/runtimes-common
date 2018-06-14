4cbff4e5356b9019eddf49ae160b3493c854fda1
Rahul Ravindran <rahulrv@yahoo.com>
2018-06-14 16:41:13 -0700

    verify contents

--- ftl/python/testdata/functionality_test/start.sh
+++ ftl/python/testdata/functionality_test/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -xe
 python /srv/app.py &
-wget --tries=20 localhost:8080
+wget --tries=20 localhost:8080 &
+[ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" == "barfoo" ]
