b1a65a577501e28404d5802bdf9d8ea29f611e52
Rahul Ravindran <rahulrv@yahoo.com>
2018-06-14 18:02:06 -0700

    use the if condition for verification

--- ftl/python/testdata/functionality_test/start.sh
+++ ftl/python/testdata/functionality_test/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -xe
-[ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" == "barfoo" ] &&
-python /srv/app.py &
-wget --tries=20 localhost:8080
+if [ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" = "barfoo" ]; then
+  python /srv/app.py &
+  wget --tries=20 localhost:8080
+else
+  exit 1
+fi
