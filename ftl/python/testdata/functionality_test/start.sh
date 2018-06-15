67cb51329dd17d6e9ab82639c2afcf3836691424
Rahul Ravindran <rahulrv@yahoo.com>
2018-06-14 17:22:17 -0700

    make tests work

--- ftl/python/testdata/functionality_test/start.sh
+++ ftl/python/testdata/functionality_test/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
+[ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" == "barfoo" ] &&
 python /srv/app.py &
 wget --tries=20 localhost:8080 &
-[ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" == "barfoo" ]
