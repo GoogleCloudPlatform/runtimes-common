78617a786bf3b966cd0d543a1f30d2cd5a1cfa4c
Rahul Ravindran <rahulrv@yahoo.com>
2018-06-19 14:49:04 -0700

    use ftl_python_functionality_test.yaml

--- ftl/integration_tests/ftl_python_integration_tests_yaml.py
+++ ftl/integration_tests/ftl_python_integration_tests_yaml.py
@@ -8,7 +8,7 @@ import util
 # Add directories for new tests here.
 TEST_DIRS = [
     'packages_test', 'metadata_test',
-    'python3_test', 'pipfile_test',
+    'python3_test', 'pipfile_test', 'additional_directory',
     'venv_dir_test', 'empty_descriptor_test', 'no_descriptor_test']
 
 _ST_IMAGE = ('gcr.io/gcp-runtimes/structure-test:'
@@ -41,6 +41,7 @@ def main():
     test_map['python3_test'].extend(['--python-cmd', 'python3.6'])
     test_map['python3_test'].extend(['--pip-cmd', 'python3.6 -m pip'])
     test_map['venv_dir_test'].extend(['--virtualenv-dir', '/alternate-env'])
+    test_map['additional_directory'].extend(['--additional-directory', '/workspace/ftl/python/testdata/additional_directory'])
 
     for test, args in test_map.iteritems():
         cloudbuild_yaml['steps'] += util.run_test_steps(
--- ftl/python/testdata/functionality_test/start.sh
+++ ftl/python/testdata/functionality_test/start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -xe
-if [ "$(cat /workspace/ftl/python/testdata/additional_directory/child/b.txt)" = "barfoo" ]; then
-  python /srv/app.py &
-  wget --tries=20 localhost:8080
-else
-  exit 1
-fi
+python /srv/app.py &
+wget --tries=20 localhost:8080
