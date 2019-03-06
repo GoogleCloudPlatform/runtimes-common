b6f3355e7b098f94ca352eda4b77f6e1a2932af1
Sharif Elgamal <selgamal@google.com>
2019-03-05 16:16:16 -0800

    Starting check version for Jetty image

--- appengine/reconciletags/version_check_test.py
+++ appengine/reconciletags/version_check_test.py
@@ -1,7 +1,7 @@
-"""Latest age tests.
+'''Latest age tests.
 
 Checks the build date of the image marked as latest for a repository and fails
-if it's over two weeks old."""
+if it's over two weeks old.'''
 
 import glob
 import json
@@ -23,21 +23,24 @@ runtime_to_latest_version = {
     'php': 'curl -L http://www.php.net/downloads.php',
     'nodejs': 'curl -L https://nodejs.org/dist/latest-v10.x/',
     'go1-builder': 'curl -L https://golang.org/dl',
-    "java": ("docker run -t --entrypoint /bin/bash {0} "
-             "-c \"apt-get update &> /dev/null; apt-get install -s {1}\"")
+    'openjdk': ('docker run -t --entrypoint /bin/bash {0} '
+             '-c \'apt-get update &> /dev/null; apt-get install -s {1}\''),
+    'jetty': ('curl -L https://repo1.maven.org/'
+              'maven2/org/eclipse/jetty/jetty-distribution/'),
 
 }
 
 runtime_to_current_version = {
-    "aspnetcore": "dotnet --info",
-    "debian": "apt-get update; apt-get -y install lsb-release; lsb_release -a",
-    "ubuntu": "apt-get update; apt-get -y install lsb-release; lsb_release -a",
-    "ruby": "ruby -v",
-    "python": "python3 --version",
-    "php": "php -v",
-    "nodejs": "node --version",
-    "java": "java -version",
-    "go1-builder": "echo $GO_VERSION"
+    'aspnetcore': 'dotnet --info',
+    'debian': 'apt-get update; apt-get -y install lsb-release; lsb_release -a',
+    'ubuntu': 'apt-get update; apt-get -y install lsb-release; lsb_release -a',
+    'ruby': 'ruby -v',
+    'python': 'python3 --version',
+    'php': 'php -v',
+    'nodejs': 'node --version',
+    'openjdk': 'java -version',
+    'go1-builder': 'echo $GO_VERSION',
+    'jetty': 'echo $JETTY_VERSION'
 }
 
 
@@ -99,7 +102,7 @@ class VersionCheckTest(unittest.TestCase):
             v.sort(key=LooseVersion)
             return v[-1]
 
-    def filter_java(s, current, version=None):
+    def filter_openjdk(s, current, version=None):
         if current:
             return re.findall(r'OpenJDK Runtime Environment '
                               '\(build \S+_\d+-(\S+)-\S{3}', s)[0]
@@ -112,16 +115,26 @@ class VersionCheckTest(unittest.TestCase):
         else:
             return re.findall(r'go({}(?:.\d+|beta\d+|rc\d+)?)'.format(version), s)[0]
 
+    def filter_jetty(s, current, version=None):
+        if current:
+            return s.rstrip()
+        else:
+          v = re.findall(r'{}.\d+.\d+.v\d+'.format(version.split('.')[0]), s)
+          v.sort(key=LooseVersion)
+          return v[-1]
+
+
     runtime_to_filter = {
-        "debian": filter_debian,
-        "ubuntu": filter_ubuntu,
-        "php": filter_php,
-        "nodejs": filter_node,
-        "python": filter_python,
-        "java": filter_java,
-        "ruby": filter_ruby,
-        "aspnetcore": filter_aspnetcore,
-        "go1-builder": filter_go
+        'debian': filter_debian,
+        'ubuntu': filter_ubuntu,
+        'php': filter_php,
+        'nodejs': filter_node,
+        'python': filter_python,
+        'openjdk': filter_openjdk,
+        'ruby': filter_ruby,
+        'aspnetcore': filter_aspnetcore,
+        'go1-builder': filter_go,
+        'jetty': filter_jetty
     }
 
     def _get_latest_version(self, runtime, version, image):
@@ -132,7 +145,7 @@ class VersionCheckTest(unittest.TestCase):
         return self.runtime_to_filter.get(runtime)(versions, False, version)
 
     def _get_current_version(self, runtime, project, image):
-        version_cmd = ("docker run -t --entrypoint /bin/bash {0} -c '{1}'"
+        version_cmd = ('docker run -t --entrypoint /bin/bash {0} -c \'{1}\''
                        .format(image,
                                runtime_to_current_version.get(runtime)))
 
@@ -152,6 +165,15 @@ class VersionCheckTest(unittest.TestCase):
                 runtime = os.path.splitext(runtime)[0]
                 for project in data['projects']:
                     logging.debug('Checking {}'.format(project['repository']))
+                    # We need to special case java because checking openjdk and
+                    # jetty is radically different
+                    if os.path.basename(f) == 'java.json':
+                        if project['repository'] == 'google-appengine/openjdk':
+                            runtime = 'openjdk'
+                        elif project['repository'] == 'google-appengine/jetty':
+                            runtime = 'jetty'
+
+                    print "RUNTIME="+runtime
                     for image in project['images']:
                         if 'check_version' in image:
                             img_name = os.path.join(project['base_registry'],
@@ -173,18 +195,18 @@ class VersionCheckTest(unittest.TestCase):
                             latest_version = self._get_latest_version(runtime,
                                                                       version,
                                                                       img_name)
-                            logging.debug("Current version: {0},"
-                                          "Latest Version: {1}"
+                            logging.debug('Current version: {0},'
+                                          'Latest Version: {1}'
                                           .format(c_version,
                                                   latest_version))
                             if latest_version != c_version:
                                 name = (project['repository']
-                                        + ":"
+                                        + ':'
                                         + image['tag'])
                                 entry = {
-                                    "image": name,
-                                    "current_version": c_version,
-                                    "latest_version": latest_version
+                                    'image': name,
+                                    'current_version': c_version,
+                                    'latest_version': latest_version
                                 }
                                 old_images.append(entry)
                 if old_images:
